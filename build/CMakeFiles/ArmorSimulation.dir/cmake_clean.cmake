2309738210b12d4fdfe851542dcf73a2034b63a8
SyafiqKarmen <sadhyaraka@gmail.com>
2025-11-06 21:27:24 +0900

    added for open source purposes

---
+++ build/CMakeFiles/ArmorSimulation.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "ArmorSimulation.exe"
+  "ArmorSimulation.exe.manifest"
+  "ArmorSimulation.pdb"
+  "CMakeFiles/ArmorSimulation.dir/src/Main.cpp.obj"
+  "CMakeFiles/ArmorSimulation.dir/src/Main.cpp.obj.d"
+  "libArmorSimulation.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ArmorSimulation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
